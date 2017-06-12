906fbd9124ba5275ace8cbb4559f665355d49d86
zhangmengjie95 <2285982508@qq.com>
2017-06-13 07:14:29 +0800

    new file

--- main.js
+++ main.js
@@ -16,19 +16,43 @@ var connection=mysql.createConnection({
 connection.connect();
 app.get("/load",function(req,res){
 	
-	 connection.query("select * from list",function(err,data){ 
-	   
+	 connection.query("select * from lists",function(err,data){ 
 	    if(data){
 	    	res.send({code:0,data:data}) 
-	    	
 	    }
+	 }) 
+});
+app.post("/add",function(req,res){
+	 var title=req.body.title,
+	     text=req.body.text,
+	     num=req.body.num;
+	   
+  connection.query("select * from lists where title='"+title+"'",function(err,data){ 
+	        
+	     if(data.length==0){
+	
+	     	 connection.query("insert into lists(title,content,num) values('"+title+"','"+text+"','"+num+"')",function(err,data){
+	     	 	    
+	     	 	  	 res.send({code:0}) 
+	     	 	  
+	     	 })
+	     	 
+	     }
+	     
 	  
 	 
 	 })
-	 
-})
+	  
+});
+app.post("/set",function(req,res){
+	   var sum=req.body.sum;
+	   var txt=req.body.txt;
+	       
+	 connection.query("update lists set num='"+sum+"' where title='"+txt+"' ",function(err,data){ 
+	     console.log(data)
+	 }) 
+}); 
 
-
-app.listen(8090,function(){
-	 console.log(8090)
+app.listen(8088,function(){
+	 console.log(8088)
 });
